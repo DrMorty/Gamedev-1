2271f8790f8835a0d3216098009d89b979767104
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-18 17:27:00 +0300

    Update eventmanager.cpp

--- src/core/management/eventmanager.cpp
+++ src/core/management/eventmanager.cpp
@@ -5,27 +5,4 @@
 #include "../engine.h"
 #include "eventmanager.h"
 
-EventManager::EventManager()
-{
-
-}
-
-
-
-void EventManager::Execute()
-{
-
-    sf::Event event;
-    while(Engine::instance()->drawManager->window.pollEvent(event))
-    {
-
-        /*
-        auto action = (ActionBinder.find(event.type));
-        if (action != ActionBinder.end())
-        {
-            (Engine::instance()->eventHandler->*action->second)();
-        }
-        */
-    }
-
-}
+EventManager::EventManage
