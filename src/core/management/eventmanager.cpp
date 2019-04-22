99bc59568a39ee16c060540d6d6406d94f5bb1d2
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-22 18:01:45 +0300

    Update eventmanager.cpp

--- src/core/management/eventmanager.cpp
+++ src/core/management/eventmanager.cpp
@@ -4,5 +4,39 @@
 #include <algorithm>
 #include "../engine.h"
 #include "eventmanager.h"
+#include "Datastorage.h"
 
-EventManager::EventManage
+namespace engine
+{
+
+  
+  void EventManager::checkEvents()
+  {
+    sf::Event event;
+    
+    while((Datadtorage->window()).pollEvent(event))
+    {
+      if (event.type == sf::Event::KeyPressed)
+      {
+        KeyboardInput::keyPressed(event.key.code)
+      };
+      
+      newEvents.push_back(event);
+    }
+  }
+  
+  void EventManager::resetEvents()
+  {
+    newEvents.clear();
+  }
+  
+  std::vector<sf::Event> EventManager::getEvents(sf::Event::EventType type)
+  {
+    std::vector<sf::Event> events;
+    
+    for (auto event : newEvents)
+      if (type == event.type)
+        events.push_back(event);
+    return events;
+  }
+}
